2d53baa97781debd2a26b6a317d47353f2010b95
seilylook <seilylook@naver.com>
2023-03-08 14:41:30 +0900

    FEAT: POST createPost method

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
 
@@ -10,5 +10,17 @@ export class BoardsController {
   @Get('/')
   getAllBoard(): Board[] {
     return this.boardService.getAllBoards();
+  }
+
+  // 게시물 작성하기
+  // express 에서는
+  // router.post('/', (req, res) => {
+  // })
+  @Post('/')
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardService.createBoard(title, description);
   }
 }
