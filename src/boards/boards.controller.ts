44115dd788b19a82dc5068e84f23060ce88ddfba
seilylook <seilylook@naver.com>
2023-03-08 15:05:45 +0900

    REFACTOR: POST createBoard with DTO(data transfer object)

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -17,10 +18,7 @@ export class BoardsController {
   // router.post('/', (req, res) => {
   // })
   @Post('/')
-  createBoard(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Board {
-    return this.boardService.createBoard(title, description);
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardService.createBoard(createBoardDto);
   }
 }
